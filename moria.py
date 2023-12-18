aee487a173bcc45a134ff1d5a888dadb4164f15d
NokkenQuant <154093795+NokkenQuant@users.noreply.github.com>
2023-12-18 17:46:38 -0300

    Update moria.py

--- moria.py
+++ moria.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import plotly.graph_objects as go
+import plotly.express as px
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import warnings
@@ -21,15 +22,18 @@ def home():
 
 def backtest():
     st.title('Resultados Backtest')
+    st.header('Rentabilidade do Fundo vs CDI')
+    
     comparacao = pd.read_csv('comparacao.csv')
     comparacao.index = comparacao.DATA
     comparacao.drop(columns='DATA', inplace=True)
     lsita_datas = list(comparacao.index)
   
     data = st.select_slider('Selecione data inicial e final',options = lsita_datas, value=(lsita_datas[0],lsita_datas[-1]))
+    st.subheader('Rentabilidade Acumulada no perído')
     comaparacao_datas = comparacao.loc[data[0]:data[1]]
     comaparacao_datas = comaparacao_datas/comaparacao_datas.iloc[0]
-    # st.write(comaparacao_datas        )
+    
     fig = go.Figure()
     fig.add_trace(go.Scatter(name = 'CDI', x= comaparacao_datas.index,y=comaparacao_datas['CDI'], line= dict(color = 'rgb(42,255,57)')))
     fig.add_trace(go.Scatter(name = 'Fundo', x= comaparacao_datas.index,y=comaparacao_datas['Fundo'], line= dict(color = 'rgb(58,25,223)')))
@@ -37,7 +41,7 @@ def backtest():
     st.plotly_chart(fig)
 
     st.markdown('---')
-
+    st.subheader('Comparação da rentabilidade do fundo contra a o CDI, para cada período')
     
     comaparacao_datas.index = pd.to_datetime(comaparacao_datas.index)
     comaparacao_datas['Ano'] = comaparacao_datas.index.year
@@ -69,6 +73,7 @@ def backtest():
     st.plotly_chart(fig2)
 
     st.markdown('---')
+    st.subheader('Sensibilidade de performance do Fundo vs CDI')
 
     sensibilidade_cdi = comaparacao_datas[['CDI', 'Fundo']]
 
@@ -86,12 +91,43 @@ def backtest():
     fig3, ax = plt.subplots(figsize = (12,10)) #cria 2 figuras
     cmap = sns.color_palette('RdYlGn', 50) # define as cores 
     sns.heatmap(cdi_pl, cmap = cmap, annot = True, fmt = '.2%', center = 0, vmax = 0.02, vmin = -0.02, cbar = False, linewidths=1, xticklabels = True, yticklabels = True, ax = ax)
-    ax.set_title('CDI vs PL do Fundo', fontsize = 18)
+    # ax.set_title('CDI vs PL do Fundo', fontsize = 18)
     ax.set_yticklabels(ax.get_yticklabels(), rotation = 0, verticalalignment = 'center', fontsize = '12')
     ax.set_xticklabels(ax.get_xticklabels(), fontsize = '12')
     ax.xaxis.tick_top() #colocar o x axis em cima
     plt.ylabel('')
     st.pyplot(fig3)
+
+    st.markdown('---')
+    st.header('Análise da Volatilidade')
+    st.subheader('Distribuição da Volatilidade do Fundo')
+    st.write('A volatilidade é calculada como desvio-padrão dos retornos diários da cota do fundo')
+
+    df_volatilidade = (comaparacao_datas['Fundo'].pct_change().rolling(21).std()*np.sqrt(252)).fillna(0)
+    
+    # Plotar o histograma
+    
+    fig4 = plt.figure(figsize=(10, 6))
+    sns.set_style("whitegrid")
+    sns.histplot(df_volatilidade, kde=True, color='#0B58DC', bins=80) 
+    plt.title('Distribuição de Dados')
+    plt.xlabel('Valores')
+    plt.ylabel('Frequência')
+    st.pyplot(fig4)
+
+    st.markdown('---')
+    st.subheader('Volatilidade ao longo do tempo')
+    
+    df_volatilidade = pd.DataFrame(df_volatilidade)
+    df_volatilidade.columns = ['Volatilidade do Fundo']
+    df_volatilidade['Vol Média'] = df_volatilidade['Volatilidade do Fundo'].mean()
+    df_volatilidade['Vol Min'] = 0.05
+    df_volatilidade['Vol Target'] = 0.1
+    df_volatilidade['Vol Max'] = 0.12
+    # mensagem = f"A Volatilidade Target é de {df_volatilidade['Vol Max']}%, a Média é de {df_volatilidade['Vol Max']}%, a Mínima é de {df_volatilidade['Vol Max']%} e a Máxima é de {df_volatilidade['Vol Max']}%".format(df_volatilidade['Vol Target'],df_volatilidade['Vol Média'],df_volatilidade['Vol Min'],df_volatilidade['Vol Max'])
+    # st.write(mensagem)
+    st.plotly_chart(px.line(df_volatilidade[['Volatilidade do Fundo', 'Vol Média','Vol Target','Vol Min', 'Vol Max']], title='Volatilidade do Fundo<br><sup>Volatilidade Móvel 21, anualizada</sup>'))
+
      
 
 def refazer():
