1521a62fff73650df98921c6989d6c376236d501
NokkenQuant <154093795+NokkenQuant@users.noreply.github.com>
2025-07-20 14:02:15 -0300

    Add files via upload

--- moria.py
+++ moria.py
@@ -63,175 +63,175 @@ def home():
     """
     st.markdown(html_code, unsafe_allow_html = True)
 
-def backtest():
-    st.title('Resultados Backtest')
-    st.header('Rentabilidade do Fundo vs CDI')
+# def backtest():
+#     st.title('Resultados Backtest')
+#     st.header('Rentabilidade do Fundo vs CDI')
     
-    comparacao = pd.read_csv('comparacao backup.csv')
-    comparacao.index = comparacao.DATA
-    comparacao.drop(columns='DATA', inplace=True)
-    lsita_datas = list(comparacao.index)
-    # st.write(comparacao)
+#     comparacao = pd.read_csv('comparacao backup.csv')
+#     comparacao.index = comparacao.DATA
+#     comparacao.drop(columns='DATA', inplace=True)
+#     lsita_datas = list(comparacao.index)
+#     # st.write(comparacao)
 
-    ############################################################################################################################################################################################
+#     ############################################################################################################################################################################################
     
-    data = st.select_slider('Selecione data inicial e final',options = lsita_datas, value=(lsita_datas[0],lsita_datas[-1]))
-    st.subheader('Rentabilidade Acumulada no perído')
-    comparacao_datas = comparacao.loc[data[0]:data[1]]
-    comparacao_datas = comparacao_datas/comparacao_datas.iloc[0]
+#     data = st.select_slider('Selecione data inicial e final',options = lsita_datas, value=(lsita_datas[0],lsita_datas[-1]))
+#     st.subheader('Rentabilidade Acumulada no perído')
+#     comparacao_datas = comparacao.loc[data[0]:data[1]]
+#     comparacao_datas = comparacao_datas/comparacao_datas.iloc[0]
 
     
-    taxa_adm =0.75
+#     taxa_adm =0.75
 
  
-    custo = ((1+(taxa_adm/100))**(1/252))
+#     custo = ((1+(taxa_adm/100))**(1/252))
        
-    comparacao_datas['Cota do Fundo'] = ((1+comparacao_datas['Cota do Fundo'].pct_change())/custo).fillna(1).cumprod()
-    # comparacao_datas.drop(columns= ['Fundo'], inplace=True)
+#     comparacao_datas['Cota do Fundo'] = ((1+comparacao_datas['Cota do Fundo'].pct_change())/custo).fillna(1).cumprod()
+#     # comparacao_datas.drop(columns= ['Fundo'], inplace=True)
     
-    retorno_acumulado = comparacao_datas['Cota do Fundo'][-1]
-    retorno_acumulado_cdi = comparacao_datas['CDI'][-1]
+#     retorno_acumulado = comparacao_datas['Cota do Fundo'][-1]
+#     retorno_acumulado_cdi = comparacao_datas['CDI'][-1]
 
 
-    st.write(f'Retorno Acumulado do Fundo **{round(retorno_acumulado*100,2)}%**')
-    st.write(f'Retorno Acumulado do CDI **{round(retorno_acumulado_cdi*100,2)}%**')
+#     st.write(f'Retorno Acumulado do Fundo **{round(retorno_acumulado*100,2)}%**')
+#     st.write(f'Retorno Acumulado do CDI **{round(retorno_acumulado_cdi*100,2)}%**')
 
-    st.markdown('')
-    st.markdown(f'***Taxa de adm aplicada: {taxa_adm}%***') 
-    st.markdown('')
+#     st.markdown('')
+#     st.markdown(f'***Taxa de adm aplicada: {taxa_adm}%***') 
+#     st.markdown('')
 
-    benchmarks = ['CDI', 'IPCA', 'IPCA+6%','IBOV']
-    multselecao = st.multiselect('Selecione aqui',benchmarks)
-    lista_bench = multselecao
-    lista_bench.append('Cota do Fundo')
-    # st.write(lista_bench)
+#     benchmarks = ['CDI', 'IPCA', 'IPCA+6%','IBOV']
+#     multselecao = st.multiselect('Selecione aqui',benchmarks)
+#     lista_bench = multselecao
+#     lista_bench.append('Cota do Fundo')
+#     # st.write(lista_bench)
     
-    if multselecao == []:
-        figx = px.line(comparacao_datas['Cota do Fundo'])
-        st.plotly_chart(figx)
-    else:
+#     if multselecao == []:
+#         figx = px.line(comparacao_datas['Cota do Fundo'])
+#         st.plotly_chart(figx)
+#     else:
                
-        figx = px.line(comparacao_datas[lista_bench])
-        st.plotly_chart(figx)
+#         figx = px.line(comparacao_datas[lista_bench])
+#         st.plotly_chart(figx)
         
-    # fig = go.Figure()
-    # fig.add_trace(go.Scatter(name = 'CDI', x= comparacao_datas.index,y=comparacao_datas['CDI'], line= dict(color = 'rgb(42,255,57)')))
-    # fig.add_trace(go.Scatter(name = 'Fundo', x= comparacao_datas.index,y=comparacao_datas['Fundo'], line= dict(color = 'rgb(58,25,223)')))
-    # fig.update_layout(title = 'Resultado Acumulado', height = 600 , width = 650)
-    # st.plotly_chart(fig)
+#     # fig = go.Figure()
+#     # fig.add_trace(go.Scatter(name = 'CDI', x= comparacao_datas.index,y=comparacao_datas['CDI'], line= dict(color = 'rgb(42,255,57)')))
+#     # fig.add_trace(go.Scatter(name = 'Fundo', x= comparacao_datas.index,y=comparacao_datas['Fundo'], line= dict(color = 'rgb(58,25,223)')))
+#     # fig.update_layout(title = 'Resultado Acumulado', height = 600 , width = 650)
+#     # st.plotly_chart(fig)
 
-    st.markdown('---')
-    st.subheader('Comparação da rentabilidade do fundo contra a o CDI, para cada período')
+#     st.markdown('---')
+#     st.subheader('Comparação da rentabilidade do fundo contra a o CDI, para cada período')
     
-    comparacao_datas.index = pd.to_datetime(comparacao_datas.index)
-    comparacao_datas = comparacao_datas.fillna(method='ffill')
-    comparacao_datas['Ano'] = comparacao_datas.index.year
+#     comparacao_datas.index = pd.to_datetime(comparacao_datas.index)
+#     comparacao_datas = comparacao_datas.fillna(method='ffill')
+#     comparacao_datas['Ano'] = comparacao_datas.index.year
 
   
 
-    # Função para calcular a razão entre o último e o primeiro valor de um grupo
-    def calcular_razao(group, ativo):
-        ultimo_valor = group.iloc[-1][ativo]
-        primeiro_valor = group.iloc[0][ativo]
-        return ultimo_valor / primeiro_valor
+#     # Função para calcular a razão entre o último e o primeiro valor de um grupo
+#     def calcular_razao(group, ativo):
+#         ultimo_valor = group.iloc[-1][ativo]
+#         primeiro_valor = group.iloc[0][ativo]
+#         return ultimo_valor / primeiro_valor
 
-    # Calcular a razão para cada ano
-    razoes_por_pl = comparacao_datas.groupby('Ano').apply(calcular_razao, 'Cota do Fundo')
-    razoes_por_cdi = comparacao_datas.groupby('Ano').apply(calcular_razao, 'CDI')
-    # razoes_por_plgatilho = comparacao_datas.groupby('Ano').apply(calcular_razao, 'PL Gatilhado')
+#     # Calcular a razão para cada ano
+#     razoes_por_pl = comparacao_datas.groupby('Ano').apply(calcular_razao, 'Cota do Fundo')
+#     razoes_por_cdi = comparacao_datas.groupby('Ano').apply(calcular_razao, 'CDI')
+#     # razoes_por_plgatilho = comparacao_datas.groupby('Ano').apply(calcular_razao, 'PL Gatilhado')
 
-    razoes_por_cdi = round((razoes_por_cdi-1),4)
-    razoes_por_pl = round((razoes_por_pl-1),4)
-    # razoes_por_plgatilho = round((razoes_por_plgatilho-1),2)
-    # Criar um gráfico de barras lado a lado
-    fig2 = go.Figure()
+#     razoes_por_cdi = round((razoes_por_cdi-1),4)
+#     razoes_por_pl = round((razoes_por_pl-1),4)
+#     # razoes_por_plgatilho = round((razoes_por_plgatilho-1),2)
+#     # Criar um gráfico de barras lado a lado
+#     fig2 = go.Figure()
 
-    fig2.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_pl,text = razoes_por_pl,textposition='auto', name='Cota do Fundo', marker_color = 'rgb(58,25,233)'))
-    fig2.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_cdi,text = razoes_por_cdi,textposition='auto', name='CDI', marker_color = 'rgb(42,255,57)'))
-    # fig.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_plgatilho,text = razoes_por_plgatilho,textposition='auto', name='PL Pos Gatilho'))
+#     fig2.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_pl,text = razoes_por_pl,textposition='auto', name='Cota do Fundo', marker_color = 'rgb(58,25,233)'))
+#     fig2.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_cdi,text = razoes_por_cdi,textposition='auto', name='CDI', marker_color = 'rgb(42,255,57)'))
+#     # fig.add_trace(go.Bar(x=razoes_por_pl.index, y=razoes_por_plgatilho,text = razoes_por_plgatilho,textposition='auto', name='PL Pos Gatilho'))
 
-    # Atualizar o layout do gráfico
-    fig2.update_layout(yaxis_tickformat = '2%f')
-    fig2.update_traces(texttemplate = '%{text:.2%}',textposition = 'auto')
+#     # Atualizar o layout do gráfico
+#     fig2.update_layout(yaxis_tickformat = '2%f')
+#     fig2.update_traces(texttemplate = '%{text:.2%}',textposition = 'auto')
 
-    fig2.update_layout(height = 600,width = 600,barmode='group', xaxis_title='Ano', yaxis_title='Razão (Último/Primeiro Valor)',
-                    title='Comparação Retorno YoY ')
-    st.plotly_chart(fig2)
+#     fig2.update_layout(height = 600,width = 600,barmode='group', xaxis_title='Ano', yaxis_title='Razão (Último/Primeiro Valor)',
+#                     title='Comparação Retorno YoY ')
+#     st.plotly_chart(fig2)
 
-    st.markdown('---')
-    st.subheader('Sensibilidade de performance do Fundo vs CDI')
+#     st.markdown('---')
+#     st.subheader('Sensibilidade de performance do Fundo vs CDI')
 
-    sensibilidade_cdi = comparacao_datas[['CDI', 'Cota do Fundo']]
+#     sensibilidade_cdi = comparacao_datas[['CDI', 'Cota do Fundo']]
 
-    sensibilidade_cdi = sensibilidade_cdi.groupby([sensibilidade_cdi.index.year.rename('Ano'), sensibilidade_cdi.index.month.rename('Mes')]).mean()#.pct_change().dropna()
+#     sensibilidade_cdi = sensibilidade_cdi.groupby([sensibilidade_cdi.index.year.rename('Ano'), sensibilidade_cdi.index.month.rename('Mes')]).mean()#.pct_change().dropna()
 
-    sensibilidade_cdi = sensibilidade_cdi.pct_change().dropna()
+#     sensibilidade_cdi = sensibilidade_cdi.pct_change().dropna()
 
-    sensibilidade_cdi['Diff'] =( (1+sensibilidade_cdi['Cota do Fundo']) / (1+sensibilidade_cdi['CDI']))-1
+#     sensibilidade_cdi['Diff'] =( (1+sensibilidade_cdi['Cota do Fundo']) / (1+sensibilidade_cdi['CDI']))-1
 
-    cdi_pl = pd.DataFrame(sensibilidade_cdi['Diff'])
-    cdi_pl =  pd.pivot_table(cdi_pl, index= 'Mes', columns= 'Ano',values= 'Diff').fillna(0)
-    cdi_pl.index = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']
+#     cdi_pl = pd.DataFrame(sensibilidade_cdi['Diff'])
+#     cdi_pl =  pd.pivot_table(cdi_pl, index= 'Mes', columns= 'Ano',values= 'Diff').fillna(0)
+#     cdi_pl.index = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']
 
-    #criacao do heatmap
-    fig3, ax = plt.subplots(figsize = (12,10)) #cria 2 figuras
-    cmap = sns.color_palette('RdYlGn', 50) # define as cores 
-    sns.heatmap(cdi_pl, cmap = cmap, annot = True, fmt = '.2%', center = 0, vmax = 0.02, vmin = -0.02, cbar = False, linewidths=1, xticklabels = True, yticklabels = True, ax = ax)
-    # ax.set_title('CDI vs PL do Fundo', fontsize = 18)
-    ax.set_yticklabels(ax.get_yticklabels(), rotation = 0, verticalalignment = 'center', fontsize = '12')
-    ax.set_xticklabels(ax.get_xticklabels(), fontsize = '12')
-    ax.xaxis.tick_top() #colocar o x axis em cima
-    plt.ylabel('')
-    st.pyplot(fig3)
+#     #criacao do heatmap
+#     fig3, ax = plt.subplots(figsize = (12,10)) #cria 2 figuras
+#     cmap = sns.color_palette('RdYlGn', 50) # define as cores 
+#     sns.heatmap(cdi_pl, cmap = cmap, annot = True, fmt = '.2%', center = 0, vmax = 0.02, vmin = -0.02, cbar = False, linewidths=1, xticklabels = True, yticklabels = True, ax = ax)
+#     # ax.set_title('CDI vs PL do Fundo', fontsize = 18)
+#     ax.set_yticklabels(ax.get_yticklabels(), rotation = 0, verticalalignment = 'center', fontsize = '12')
+#     ax.set_xticklabels(ax.get_xticklabels(), fontsize = '12')
+#     ax.xaxis.tick_top() #colocar o x axis em cima
+#     plt.ylabel('')
+#     st.pyplot(fig3)
 
-    st.markdown('---')
-    st.header('Análise da Volatilidade')
-    st.subheader('Distribuição da Volatilidade do Fundo')
-    st.write('A volatilidade é calculada como desvio-padrão dos retornos diários da cota do fundo')
+#     st.markdown('---')
+#     st.header('Análise da Volatilidade')
+#     st.subheader('Distribuição da Volatilidade do Fundo')
+#     st.write('A volatilidade é calculada como desvio-padrão dos retornos diários da cota do fundo')
 
-    df_volatilidade = (comparacao_datas['Cota do Fundo'].pct_change().rolling(21).std()*np.sqrt(252))
-    # st.write(df_volatilidade)
+#     df_volatilidade = (comparacao_datas['Cota do Fundo'].pct_change().rolling(21).std()*np.sqrt(252))
+#     # st.write(df_volatilidade)
     
-    # Plotar o histograma
-    
-
-    fig4 = plt.figure(figsize=(10, 6))
-    sns.set_style("whitegrid")
-    sns.histplot(df_volatilidade, kde=True, color='#0B58DC', bins=80) 
-
-    # Formatando o eixo X em percentual
-    plt.gca().xaxis.set_major_formatter(PercentFormatter(1, decimals=0))
-
-    plt.title('Distribuição de Dados')
-    plt.xlabel('Valores (em percentual)')
-    plt.ylabel('Frequência')
-
-    st.pyplot(fig4)
-
-    st.markdown('---')
-    st.subheader('Volatilidade ao longo do tempo')
-    
-    df_volatilidade = pd.DataFrame(df_volatilidade)
-    df_volatilidade.columns = ['Volatilidade do Fundo']
-    df_volatilidade['Vol Média'] = df_volatilidade['Volatilidade do Fundo'].mean()
-    df_volatilidade['Vol Min'] = 0.05
-    df_volatilidade['Vol Target'] = 0.1
-    df_volatilidade['Vol Max'] = 0.12
-    df_volatilidade['Volatilidade do Fundo'] = df_volatilidade['Volatilidade do Fundo'].fillna(method='bfill')
-
-    # Criando o gráfico de linha com Plotly Express
-    fig = px.line(df_volatilidade[['Volatilidade do Fundo', 'Vol Média', 'Vol Target', 'Vol Min', 'Vol Max']],
-                title='Volatilidade do Fundo<br><sup>Volatilidade Móvel 21, anualizada</sup>')
-
-    # Formatando o eixo Y como percentual
-    fig.update_layout(yaxis_tickformat=".2%")
+#     # Plotar o histograma
     
 
-    # Adicionando uma referência ao percentual no eixo Y (opcional)
-    # fig.update_layout(annotations=[dict(xref='paper', yref='paper', x=0.02, y=0.98)])
+#     fig4 = plt.figure(figsize=(10, 6))
+#     sns.set_style("whitegrid")
+#     sns.histplot(df_volatilidade, kde=True, color='#0B58DC', bins=80) 
 
-    # Mostrando o gráfico
-    st.plotly_chart(fig)
+#     # Formatando o eixo X em percentual
+#     plt.gca().xaxis.set_major_formatter(PercentFormatter(1, decimals=0))
+
+#     plt.title('Distribuição de Dados')
+#     plt.xlabel('Valores (em percentual)')
+#     plt.ylabel('Frequência')
+
+#     st.pyplot(fig4)
+
+#     st.markdown('---')
+#     st.subheader('Volatilidade ao longo do tempo')
+    
+#     df_volatilidade = pd.DataFrame(df_volatilidade)
+#     df_volatilidade.columns = ['Volatilidade do Fundo']
+#     df_volatilidade['Vol Média'] = df_volatilidade['Volatilidade do Fundo'].mean()
+#     df_volatilidade['Vol Min'] = 0.05
+#     df_volatilidade['Vol Target'] = 0.1
+#     df_volatilidade['Vol Max'] = 0.12
+#     df_volatilidade['Volatilidade do Fundo'] = df_volatilidade['Volatilidade do Fundo'].fillna(method='bfill')
+
+#     # Criando o gráfico de linha com Plotly Express
+#     fig = px.line(df_volatilidade[['Volatilidade do Fundo', 'Vol Média', 'Vol Target', 'Vol Min', 'Vol Max']],
+#                 title='Volatilidade do Fundo<br><sup>Volatilidade Móvel 21, anualizada</sup>')
+
+#     # Formatando o eixo Y como percentual
+#     fig.update_layout(yaxis_tickformat=".2%")
+    
+
+#     # Adicionando uma referência ao percentual no eixo Y (opcional)
+#     # fig.update_layout(annotations=[dict(xref='paper', yref='paper', x=0.02, y=0.98)])
+
+#     # Mostrando o gráfico
+#     st.plotly_chart(fig)
 
 def backtest_etf():
     st.title('Resultados Backtest com Indices')
@@ -418,8 +418,10 @@ def backtest_etf():
     z_score_min = ((vol_min - vol_media) / vol_desv_pad)
 
     vol_dist = norm.cdf(z_score_max-z_score_min)
-
+    with open('analise_quali.json', 'r') as json_file:
+    analise_quali_lido = json.load(json_file)
     
+     
 
 
     analise_quali = {
@@ -427,7 +429,7 @@ def backtest_etf():
     'vol_dist': round(vol_dist * 100, 2),
     'vol_media': round(vol_media * 100, 2),
     
-    'fundos': list(comparacao.columns),
+    'fundos': analise_quali_lido['fundos'],
     'data_inicial' : comparacao.index[0],
     'data_final' : comparacao.index[-1],
     'Parametros': {
